3cd3bcf18c888b936c4484c2663ee652af759f83
Ignacio Fuentes <ignaciofuentes@outlook.com>
2021-09-30 21:12:09 -0400

    Update Program.cs

--- capstone-service-2/Program.cs
+++ capstone-service-2/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/service2", () => "Hello World 12345!");
+app.MapGet("/service2", () => "Hello World 123456!");
 
 app.Run();
